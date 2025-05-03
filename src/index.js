2eabdcc2fee8d4e1e85c14b870e293d5280d7b3d
parasol250 <vula0231@gmail.com>
2025-05-03 18:26:04 +0300

    деплой??

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';  // Изменение здесь
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+      <HashRouter>  {/* Оберните App в HashRouter */}
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
